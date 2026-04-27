b02ecaf45d21a20b780db7d26b248fd57b393784
ZhaoShop <zhaocute3@gmail.com>
2026-04-27 16:36:37 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID"
+  apiKey: "AIzaSyAjucPXbCYj_XvkDER6njOlKZs21X7a0vc",
+  authDomain: "bp-coins-seller-614ef.firebaseapp.com",
+  projectId: "bp-coins-seller-614ef"
 };
 
 const app = initializeApp(firebaseConfig);
