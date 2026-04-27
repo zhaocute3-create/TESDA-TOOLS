ead3802b54e19284725a6a153a52f548af4db857
ZhaoShop <zhaocute3@gmail.com>
2026-04-27 16:28:14 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
