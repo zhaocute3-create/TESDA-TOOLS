beac3a36fceb2ca3e5743b067468f738d69af420
ZhaoShop <zhaocute3@gmail.com>
2026-04-28 21:41:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const tools = [
   {
   name: "Laser Distance",
   desc: "Used for measuring long distances accurately using laser technology",
-  image: "Aparat-de-masurare-la-distanta-laser-0-2---60-m--IP65--Industrial-_1000x1000_1.jpg"
+  image: ""
 },
 {
   name: "Laser Level",
